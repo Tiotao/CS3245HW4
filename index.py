143f55757ea782447bb989f19fdac94ce1033658
Minqi Chen <cnchenminqi@gmail.com>
2014-04-10 04:37:58 +0800

    initial naive tf-idf attempt

---
+++ index.py
@@ -0,0 +1,224 @@
+from nltk.tokenize import word_tokenize, sent_tokenize
+from nltk.stem.porter import *
+from nltk.corpus import stopwords
+
+from os import walk
+import sys, json, getopt, math, string
+sys.path.append("./ET/elementtree/")
+import ElementTree as ET
+#######################################################################
+# Sample command:
+#	python index.py -i D:\training\ -d dictionary.txt -p postings.txt
+#######################################################################
+
+
+#######################################################################
+# Read filenames - Creating posting lists - Generate outputs
+#######################################################################
+
+def index(docs_dir, dict_file, posting_file):
+	print 'start indexing'	
+
+	filenames = []
+	for (root, dirs, files) in walk(docs_dir):
+		files = [ (x) for x in files ]
+		filenames.extend(files)
+	filenames.sort()
+
+	posting_array_dict = generate_posting_array_dict(filenames)
+	print '\nstart calculating document vector lengths'
+
+	lengths_dict = generate_document_vector_length(filenames, posting_array_dict)
+	print "\nstart processing output"
+
+	generate_output(posting_array_dict, lengths_dict, dict_file, posting_file)
+
+	print "\nmission completed :)"
+
+
+def generate_output(dict, lengths, dict_file, posting_file):
+	
+	# init stdout
+	curr = 0
+	
+	# init tokens list (sorted) and the list length
+	sorted_keys = sorted(dict.iterkeys())
+	total = len(sorted_keys)
+	
+	# init content for dictionary file
+	dict_content = ""
+
+	# init content for postings file
+	# header: file lengths
+	posting_header = json.dumps(lengths) + '\n'
+	posting_f = open(posting_file, 'wb')
+	posting_f.write(posting_header)
+	
+	# preparing content to be written into files
+	for key in sorted_keys:
+		posting_pos = write_posting(dict[key], posting_f)
+		dict_line = "%(token)s %(length)s %(start)s %(end)s\n"%{'token':str(key), 'length':str(len(dict[key])), 'start':str(posting_pos[0]), 'end':str(posting_pos[1])}
+		dict_content = dict_content + dict_line
+		curr += 1
+		sys.stdout.write("\rprocessing: " + ("%.2f" % (100.0 * curr / total)) + '%')
+		sys.stdout.flush()
+
+	# finished writing positing file 
+	posting_f.close()
+
+	# write into dictionary file
+	dict_f = open(dict_file, 'wb')
+	dict_f.write(str(dict_content))
+	dict_f.close()
+
+# build SkipList for every tokens from its posting array, build skips.
+# pickled each SkipList into string and write into posting files. 
+# return starting and ending position of the SkipList in the positng file
+
+def write_posting(posting_dict, posting_f):
+	posting_start = posting_f.tell()
+	posting_f.write(json.dumps(posting_dict) + '\n')
+	posting_end = posting_f.tell()
+	return posting_start, posting_end
+
+#######################################################################
+# Generating Posting SkipLists
+#######################################################################
+
+# returns a dictionary
+# key: tokens
+# value: posting array
+def generate_posting_array_dict(filenames):
+
+	# init stdout 
+	curr = 0
+
+	# init empty posting array 
+	posting_array_dict = {}
+	
+	# totol length of files to be processed 
+	total = len(filenames)
+	for filename in filenames:
+		# genearate tokenized and stemmed (stopwords and numbers can be filtered by option) from a file
+		try:
+			tokens = generate_tokens(str(filename))
+			# store tokens and filnames into posting_array_dict
+			# append filenames at end of the posting array if token key existed in the dictionary
+			for t in tokens:
+				key = str(t)
+				if key in posting_array_dict:
+					if filename in posting_array_dict[key]:
+						posting_array_dict[key][filename] = posting_array_dict[key][filename] + 1
+					if not filename in posting_array_dict[key]:
+						posting_array_dict[key][filename] = 1
+				else:
+					posting_array_dict[key] = {filename: 1}
+
+			curr += 1
+			sys.stdout.write("\rindexing: " + ("%.2f" % (100.0 * curr / total)) + '%')
+			sys.stdout.flush()
+		except ET.ParseError:
+			pass
+	return posting_array_dict
+
+def generate_document_vector_length(filenames, dict):
+	# init stdout 
+	curr = 0
+	total = len(filenames)
+	lengths_dict = {}
+
+	for filename in filenames:
+		try:
+			axis = {}
+			tokens = generate_tokens(str(filename))
+			for token in tokens:
+				if token in axis:
+					axis[token] += 1
+				else:
+					axis[token] = 1
+			sum_of_squared = 0
+			for token, freq in axis.iteritems():
+				d_tf = float(1) + math.log(freq, 10)
+				d_df = len(dict[token])
+				d_idf = math.log(float(total) / d_df, 10) if d_df != 0 else 1
+				sum_of_squared += (d_tf * d_idf) ** 2
+			lengths_dict[filename] = math.sqrt(sum_of_squared)
+			curr += 1
+			sys.stdout.write("\rprocessing: " + ("%.2f" % (100.0 * curr / total)) + '%')
+			sys.stdout.flush()
+		except ET.ParseError:
+			pass
+	return lengths_dict
+
+#######################################################################
+# Tokenization and Stemming 
+#######################################################################
+
+# generate array of tokens from file
+def generate_tokens(filename):
+	words = tokenize_sentences(filename)
+	tokens = stemming_words(words)
+	tokens = filter_stopwords(tokens)
+	return tokens
+
+# tokenize sentences in a file into array of words
+def tokenize_sentences(filename):
+	file_dir = docs_dir + str(filename)
+	f = open(file_dir, 'r')
+
+	root = ET.parse(f).getroot()
+	tags = root.getiterator('str')
+
+	# read the relevant tags
+	file_string = ''
+	for tag in tags:
+		if tag.get('name')  == 'Title' or tag.get('name') == 'Abstract':
+			file_string += filter(lambda x: x in string.printable, tag.text.lower().strip().replace('relevant documents will describe', ''))
+
+	f.close()
+
+	sentences = sent_tokenize(file_string)
+	words = []
+	for s in sentences:
+		words = words + word_tokenize(s)
+	return words
+
+stemmer = PorterStemmer()
+def stemming_words(words):
+	tokens = []
+	for w in words:
+		tokens.append(stemmer.stem(w))
+	return tokens
+
+stop = stemming_words(stopwords.words('english'))
+def filter_stopwords(words):
+	return [i for i in words if i not in stop]
+
+
+#######################################################################
+# Main
+#######################################################################
+
+def usage():
+    print "usage: " + sys.argv[0] + " -i directory-of-documents -d dictionary-file -p postings-file"
+
+docs_dir = dict_file = posting_file = None
+try:
+    opts, args = getopt.getopt(sys.argv[1:], 'i:d:p:')
+except getopt.GetoptError, err:
+    usage()
+    sys.exit(2)
+for o, a in opts:
+    if o == '-i':
+        docs_dir = a
+    elif o == '-d':
+        dict_file = a
+    elif o == '-p':
+        posting_file = a
+    else:
+        pass # no-op
+if docs_dir == None or dict_file == None or posting_file == None:
+    usage()
+    sys.exit(2)
+
+index(docs_dir, dict_file, posting_file)
---
+++ search.py
@@ -0,0 +1,201 @@
+from nltk.stem.porter import *
+import getopt, json, math, heapq, sys, string
+from nltk.corpus import stopwords
+
+sys.path.append("./ET/elementtree/")
+import ElementTree as ET
+#######################################################################
+# Read query - execute query - output result
+#######################################################################
+
+def search():
+	query_file = file(QUERY_DIR, 'r')
+
+	root = ET.parse(query_file).getroot()
+	tags = root.getiterator()
+
+	# read the relevant tags
+	file_string = ''
+	for tag in tags:
+		if tag.tag == 'title' or tag.tag == 'description':
+			file_string += filter(lambda x: x in string.printable, tag.text.lower().strip().replace('relevant documents will describe', ''))
+
+	query_file.close()
+	print file_string
+	query = parse_query(file_string)
+	result = evaluate(query)
+	output(result)
+	query_file.close()
+
+
+def output(result):
+	out_file.write(' '.join(map(str, result)) + '\n')
+
+def parse_query(raw):
+	# after tokenising the query,
+	# we collect terms into a dictionary of term - term frequency
+	# for later calculation of query tf
+	query = raw.strip().split()
+	query = [normalise_word(w) for w in filter_stopwords(query)]
+	print query
+	query_dict = {}
+	for token in query:
+		if token in query_dict:
+			query_dict[token] += 1
+		else:
+			query_dict[token] = 1
+	return query_dict
+
+#######################################################################
+# Evaluation
+#######################################################################
+
+def evaluate(query):
+	master = {}
+	for q in query:
+		postings = lookup(q)
+		master = merge(master, postings, q)
+	# master dict is now of the following structure:
+	# {
+	#    docID1: {token1: tf1, token2: tf2, ...}
+	#    ...
+	# }
+	
+	top = []
+	for doc in master:
+		# candidate is a tuple (cosine similarity score, docID)
+		candidate = (- cos_sim(query, master[doc], DOC_LENGTHS[doc]), doc)
+		heapq.heappush(top, candidate)
+
+	result = []
+	while top:
+		candidate = heapq.heappop(top)
+		result.append(candidate)
+	sorted(result,  cmp=compare)
+	return [x[1] for x in result]
+
+# sort the candidates first by score, then by docID
+def compare(doc1, doc2):
+	if (doc1[0] == doc2[0]):
+		return cmp(doc1[1], doc2[1])
+	elif doc1[0] < doc2[0]:
+		return -1
+	elif doc1[0] < doc2[0]:
+		return 0
+	else:
+		return 1
+
+# cosine similarity caocluated by ltc.lnc
+# note that doc_length is computed during indexing
+# for better performance in normalisation
+def cos_sim(query, doc, doc_length):
+	dot_product = 0
+	q_sum_of_sqr = 0
+	for token, q_rawtf in query.iteritems():
+		q_w = float(1) + math.log(q_rawtf, 10)
+		if token not in doc:
+			d_w = 0
+		else:
+			d_tf = float(1) + math.log(doc[token], 10)
+			d_df = freq(token)
+			d_idf = math.log(float(COLLECTION_SIZE) / d_df, 10) if d_df != 0 else 1
+			d_w = d_tf*d_idf
+		dot_product += q_w * d_w
+		q_sum_of_sqr += q_w ** 2
+	return float(dot_product) / doc_length / math.sqrt(q_sum_of_sqr)
+
+#######################################################################
+# Set operations
+#######################################################################
+
+def merge(master, second, token):
+	"""
+	merge a second postings into the master postings list
+	"""
+	
+	for doc, tf in second.iteritems():
+		if doc in master:
+			master[doc][token] = tf
+		else:
+			master[doc] = {token: tf}
+	return master
+
+#######################################################################
+# Utilities
+#######################################################################
+
+# return the size of the postings for a given token
+def freq(word):
+	if word in dictionary:
+		return dictionary[word]['freq']
+	else:
+		return 0
+
+def lookup(word):
+	if word in dictionary:
+		postings_file.seek(dictionary[word]['start'])
+		raw = postings_file.read(dictionary[word]['size'])
+		return json.loads(raw)
+	else:
+		return {}
+
+stemmer = PorterStemmer()
+# case folding, stemming
+def normalise_word(word):
+	return stemmer.stem(word.lower())
+def normalise_words(words):
+	return [stemmer.stem(word.lower()) for word in words]
+
+stop = normalise_words(stopwords.words('english'))
+def filter_stopwords(words):
+	return [i for i in words if i not in stop]
+
+def usage():
+    print "usage: " + sys.argv[0] + " -d dictionary-file -p postings-file -q file-of-queries -o output-file-of-results"
+
+#######################################################################
+# Main
+#######################################################################
+
+try:
+    opts, args = getopt.getopt(sys.argv[1:], 'd:p:q:o:')
+except getopt.GetoptError, err:
+    usage()
+    sys.exit(2)
+QUERY_DIR = DICT_DIR = POSTING_DIR = OUT_DIR = None
+for o, a in opts:
+    if o == '-q':
+        QUERY_DIR = a
+    elif o == '-o':
+    	OUT_DIR = a
+    elif o == '-d':
+        DICT_DIR = a
+    elif o == '-p':
+        POSTING_DIR = a
+    else:
+        pass # no-op
+if QUERY_DIR == None or DICT_DIR == None or POSTING_DIR == None or OUT_DIR == None:
+    usage()
+    sys.exit(2)
+
+out_file = file(OUT_DIR, 'w')
+dict_file = file(DICT_DIR, 'r')
+dictionary = {}
+for line in dict_file:
+	# format: token freq start_pos end_pos
+	data = line.split()
+	dictionary[data[0]] = {
+		'freq':  int(data[1]),
+		'start': int(data[2]),
+		'size':   int(data[3]) - int(data[2])
+	}
+
+dict_file.close()
+postings_file = file(POSTING_DIR, 'r')
+DOC_LENGTHS = json.loads(postings_file.readline())
+COLLECTION_SIZE = len(DOC_LENGTHS)
+
+search()
+
+out_file.close()
+postings_file.close()
